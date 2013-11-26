4853ac391c3f45c2bd8fe1a19218ee0514b5e388
DeadDreamer <dead.dreamer@gmail.com>
2013-11-27 00:23:39 +0400

    Split BoxSample.h into header and implementation.

--- Chapter2/2_Box2D/BoxSample.h
+++ Chapter2/2_Box2D/BoxSample.h
@@ -31,79 +31,26 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-Body* CreateBody( const Vec2& Size, float Mass, float Friction )
-{
-	Body* B = new Body();
-	B->Set( Size, Mass );
-	B->friction = Friction;
+#ifndef BOX2D_BOX_SAMPLE_H
+#define BOX2D_BOX_SAMPLE_H
 
-	return B;
+// Forward declaration of Box2D types.
+namespace Box2D
+{
+	struct Vec2;
+	struct Body;
+	struct Joint;
+	struct World;
 }
 
-Body* CreateBodyPos( const Vec2& Pos, float Ang, const Vec2& Size, float Mass, float Friction )
-{
-	Body* B = CreateBody( Size, Mass, Friction );
-	B->position = Pos;
-	B->rotation = Ang;
-	return B;
-}
+Box2D::Body* CreateBody( const Box2D::Vec2& Size, float Mass, float Friction );
 
-Joint* CreateJoint( Body* B1, Body* B2, const Vec2& Anchor, float Softness, float Bias )
-{
-	Joint* J = new Joint();
+Box2D::Body* CreateBodyPos( const Box2D::Vec2& Pos, float Ang, const Vec2& Size, float Mass, float Friction );
 
-	J->Set( B1, B2, Anchor );
+Box2D::Joint* CreateJoint( Box2D::Body* B1, Box2D::Body* B2, const Box2D::Vec2& Anchor, float Softness, float Bias );
 
-	J->softness = Softness;
-	J->biasFactor = Bias;
+Box2D::Body* AddGround( Box2D::World* W, const Box2D::Vec2& Pos, float Angle, const Box2D::Vec2& Size, float Friction );
 
-	return J;
-}
+void setup3( Box2D::World* W );
 
-Body* AddGround( World* W, const Vec2& Pos, float Angle, const Vec2& Size, float Friction )
-{
-	Body* Ground = CreateBody( Size, FLT_MAX, Friction );
-
-	Ground->position = Pos;
-	Ground->rotation = Angle;
-
-	W->Add( Ground );
-
-	return Ground;
-}
-
-void setup3( World* W )
-{
-	W->gravity = Vec2( 0, -10 );
-
-	Body* G = AddGround( W, Vec2( 0.0f, -10.0f ), 0.0f, Vec2( 100.0f, 20.0f ), 0.2f );
-
-	/// 8. Dominos
-	AddGround( W, Vec2( -1.5f, 10.0f ), 0.0f, Vec2( 12.0f, 0.5f ), 0.2f );
-
-	for ( int i = 0; i < 10; ++i )
-	{ W->Add( CreateBodyPos( Vec2( -6.0f + 1.0f * ( float )i, 11.125f ), 0.0f, Vec2( 0.2f, 2.0f ), 10.0f, 0.1f ) ); }
-
-	Body* B1 = AddGround( W, Vec2( 1.0f, 6.0f ), 0.3f, Vec2( 14.0f, 0.5f ), 0.2f );
-	Body* B2 = AddGround( W, Vec2( -7.0f, 4.0f ), 0.0f, Vec2( 0.5f, 3.0f ), 0.2f );
-
-	Body* B3 = CreateBodyPos( Vec2( -0.9f, 1.0f ), 0.0f, Vec2( 12.0f, 0.25f ), 20.0f, 0.2f );
-	W->Add( B3 );
-
-	W->Add( CreateJoint( G, B3, Vec2( -2.0f, 1.0f ), 0.0f, 0.2f ) );
-
-	Body* B4 = CreateBodyPos( Vec2( -10.0f, 15.0f ), 0.0f, Vec2( 0.5f, 0.5f ), 10.0f, 0.2f );
-	W->Add( B4 );
-
-	W->Add( CreateJoint( B2, B4, Vec2( -7.0f, 15.0f ), 0.0f, 0.2f ) );
-
-	Body* B5 = CreateBodyPos( Vec2( 6.0f, 2.5f ), 0.0f, Vec2( 2.0f, 2.0f ), 20.0f, 0.1f );
-	W->Add( B5 );
-
-	W->Add( CreateJoint( B1, B5, Vec2( 6.0f, 2.6f ), 0.0f, 0.2f ) );
-
-	Body* B6 = CreateBodyPos( Vec2( 6.0f, 3.6f ), 0.0f, Vec2( 2.0f, 0.2f ), 10.0f, 0.2f );
-	W->Add( B6 );
-
-	W->Add( CreateJoint( B5, B6, Vec2( 7.0f, 3.5f ), 0.0f, 0.2f ) );
-}
+#endif // BOX2D_BOX_SAMPLE_H
